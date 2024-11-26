0efcc0e1c0237130e931a1ae7df561e64a639e93
ervitis <v-martinalonso@mercari.com>
2024-11-26 15:06:29 +0900

    feat: setup and set max column

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -283,6 +283,20 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+        },
+        ensure_installed = {
+          "vimdoc",
+          "luadoc",
+          "vim",
+          "lua",
+          "markdown",
+        },
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
