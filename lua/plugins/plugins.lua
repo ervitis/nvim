2714ed78fd4e351a0176aabf7efefe44cbe6beb3
ervitis <vitomarti@gmail.com>
2025-06-27 21:22:37 +0900

    feat: set multicurosr

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -69,6 +69,25 @@ return {
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
       end
       local lspconfig = require("lspconfig")
+      local on_attach = function(client, bufnr)
+        local keymap = vim.keymap.set
+        local opts = { buffer = bufnr }
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format()
+            vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } } })
+          end,
+        })
+
+        keymap("n", "<C-d>", vim.lsp.buf.definition, { desc = "Go to definition", unpack(opts) })
+        keymap("n", "<C-e>", vim.lsp.buf.declaration, { desc = "Go to declaration", unpack(opts) })
+        keymap("n", "<C-i>", vim.lsp.buf.implementation, { desc = "Go to implementation", unpack(opts) })
+        keymap("n", "<C-r>", vim.lsp.buf.references, { desc = "Show references", unpack(opts) })
+        keymap("n", "<C-p>", vim.lsp.buf.hover, { desc = "Peek definition", unpack(opts) })
+      end
+
       lspconfig.gopls.setup({
         settings = {
           gopls = {
@@ -87,27 +106,20 @@ return {
             },
           },
         },
-        on_attach = function(client, bufnr)
-          local keymap = vim.keymap.set
-          local opts = { buffer = bufnr }
-
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format()
-              vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } } })
-            end,
-          })
-
-          keymap("n", "<C-d>", vim.lsp.buf.definition, { desc = "Go to definition", unpack(opts) })
-          keymap("n", "<C-e>", vim.lsp.buf.declaration, { desc = "Go to declaration", unpack(opts) })
-          keymap("n", "<C-i>", vim.lsp.buf.implementation, { desc = "Go to implementation", unpack(opts) })
-          keymap("n", "<C-r>", vim.lsp.buf.references, { desc = "Show references", unpack(opts) })
-          keymap("n", "<C-p>", vim.lsp.buf.hover, { desc = "Peek definition", unpack(opts) })
-        end,
+        on_attach = on_attach,
       })
 
-      lspconfig.zls.setup({})
+      lspconfig.zls.setup({
+        settings = {
+          zls = {
+            enable_inlay_hints = true,
+            enable_snippets = true,
+            warn_style = true,
+            verbose = true,
+          },
+        },
+        on_attach = on_attach,
+      })
 
       lspconfig.pyright.setup({
         settings = {
@@ -116,6 +128,7 @@ return {
             venv = ".venv",
           },
         },
+        on_attach = on_attach,
       })
     end,
   },
@@ -141,6 +154,83 @@ return {
           ghost_text = false,
         },
       })
+    end,
+  },
+
+  -- multicursor
+  {
+    "jake-stewart/multicursor.nvim",
+    config = function()
+      local mc = require("multicursor-nvim")
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add or skip cursor above/below the main cursor.
+      set({ "n", "x" }, "<up>", function()
+        mc.lineAddCursor(-1)
+      end)
+      set({ "n", "x" }, "<down>", function()
+        mc.lineAddCursor(1)
+      end)
+      set({ "n", "x" }, "<leader><up>", function()
+        mc.lineSkipCursor(-1)
+      end)
+      set({ "n", "x" }, "<leader><down>", function()
+        mc.lineSkipCursor(1)
+      end)
+
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ "n", "x" }, "<leader>n", function()
+        mc.matchAddCursor(1)
+      end)
+      set({ "n", "x" }, "<leader>s", function()
+        mc.matchSkipCursor(1)
+      end)
+      set({ "n", "x" }, "<leader>N", function()
+        mc.matchAddCursor(-1)
+      end)
+      set({ "n", "x" }, "<leader>S", function()
+        mc.matchSkipCursor(-1)
+      end)
+
+      -- Add and remove cursors with control + left click.
+      set("n", "<c-leftmouse>", mc.handleMouse)
+      set("n", "<c-leftdrag>", mc.handleMouseDrag)
+      set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+      -- Disable and enable cursors.
+      set({ "n", "x" }, "<c-q>", mc.toggleCursor)
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one.
+        layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+        layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+        -- Delete the main cursor.
+        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+        -- Enable and clear cursors using escape.
+        layerSet("n", "<esc>", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { reverse = true })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorMatchPreview", { link = "Search" })
+      hl(0, "MultiCursorDisabledCursor", { reverse = true })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
     end,
   },
 
