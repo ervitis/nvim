db8c9d96c94732903212ca233b37921e1e42606d
ervitis <vitomarti@gmail.com>
2025-06-28 10:47:47 +0900

    fix: multicursor using option key

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -22,7 +22,16 @@ return {
   { "nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim" },
   { "akinsho/toggleterm.nvim" },
   -- Treesitter for syntax
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua" },
+        highlight = { enable = true },
+      })
+    end,
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
@@ -160,69 +169,50 @@ return {
   -- multicursor
   {
     "jake-stewart/multicursor.nvim",
+    event = "VeryLazy",
     config = function()
       local mc = require("multicursor-nvim")
       mc.setup()
 
       local set = vim.keymap.set
 
-      -- Add or skip cursor above/below the main cursor.
-      set({ "n", "x" }, "<up>", function()
-        mc.lineAddCursor(-1)
-      end)
-      set({ "n", "x" }, "<down>", function()
-        mc.lineAddCursor(1)
-      end)
-      set({ "n", "x" }, "<leader><up>", function()
-        mc.lineSkipCursor(-1)
-      end)
-      set({ "n", "x" }, "<leader><down>", function()
-        mc.lineSkipCursor(1)
-      end)
+      set("n", "<A-S-leftmouse>", mc.handleMouse, { desc = "Add cursor with Alt+Shift+Click" })
+      set("n", "<A-S-leftdrag>", mc.handleMouseDrag, { desc = "Drag with Alt+Shift+Click" })
+      set("n", "<A-S-leftrelease>", mc.handleMouseRelease, { desc = "Release with Alt+Shift+Click" })
 
-      -- Add or skip adding a new cursor by matching word/selection
-      set({ "n", "x" }, "<leader>n", function()
+      set({ "n", "x" }, "<M-g>", function()
         mc.matchAddCursor(1)
-      end)
+      end, { desc = "Add cursor to next match (IntelliJ Cmd+G)" })
+
+      set({ "n", "x" }, "<M-S-g>", function()
+        mc.deleteCursor()
+      end, { desc = "Remove last cursor (IntelliJ Option+Shift+G)" })
+
+      set({ "n", "x" }, "<M-S-Up>", function()
+        mc.lineAddCursor(-1)
+      end, { desc = "Add cursor above" })
+      set({ "n", "x" }, "<M-S-Down>", function()
+        mc.lineAddCursor(1)
+      end, { desc = "Add cursor below" })
+
       set({ "n", "x" }, "<leader>s", function()
         mc.matchSkipCursor(1)
-      end)
-      set({ "n", "x" }, "<leader>N", function()
-        mc.matchAddCursor(-1)
-      end)
-      set({ "n", "x" }, "<leader>S", function()
-        mc.matchSkipCursor(-1)
-      end)
+      end, { desc = "Skip next match" })
 
-      -- Add and remove cursors with control + left click.
-      set("n", "<c-leftmouse>", mc.handleMouse)
-      set("n", "<c-leftdrag>", mc.handleMouseDrag)
-      set("n", "<c-leftrelease>", mc.handleMouseRelease)
+      set({ "n", "x" }, "<leader>q", mc.toggleCursor, { desc = "Toggle cursors" })
 
-      -- Disable and enable cursors.
-      set({ "n", "x" }, "<c-q>", mc.toggleCursor)
-
-      -- Mappings defined in a keymap layer only apply when there are
-      -- multiple cursors. This lets you have overlapping mappings.
       mc.addKeymapLayer(function(layerSet)
-        -- Select a different cursor as the main one.
-        layerSet({ "n", "x" }, "<left>", mc.prevCursor)
-        layerSet({ "n", "x" }, "<right>", mc.nextCursor)
-
-        -- Delete the main cursor.
-        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
-
-        -- Enable and clear cursors using escape.
-        layerSet("n", "<esc>", function()
+        layerSet({ "n", "x" }, "<Left>", mc.prevCursor, { desc = "Select previous cursor" })
+        layerSet({ "n", "x" }, "<Right>", mc.nextCursor, { desc = "Select next cursor" })
+        layerSet("n", "<Esc>", function()
           if not mc.cursorsEnabled() then
             mc.enableCursors()
           else
             mc.clearCursors()
           end
-        end)
+        end, { desc = "Clear cursors" })
       end)
 
-      -- Customize how cursors look.
       local hl = vim.api.nvim_set_hl
       hl(0, "MultiCursorCursor", { reverse = true })
       hl(0, "MultiCursorVisual", { link = "Visual" })
@@ -231,6 +221,14 @@ return {
       hl(0, "MultiCursorDisabledCursor", { reverse = true })
       hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
       hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    end,
+  },
+
+  -- Which-key
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
     end,
   },
 
